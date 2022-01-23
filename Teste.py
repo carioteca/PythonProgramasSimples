85f5a8d30ff2741b92d7cf2f652812ac48b7b06d
Letícia <leticianara12@gmail.com>
2022-01-23 08:16:09 -0300

    Programa para mudar nome
    Neste programa você irá digitar o nome e sobrenome. Ele irá mostrar a apenas o nome.

---
+++ Teste.py
@@ -0,0 +1,16 @@
+import os
+import time
+
+r = 0
+while r == 0:
+    pass
+    nome = input(' Digite o nome: ')
+    pos =  nome.find(' ')
+    nome = nome[0:pos]
+    nome = nome.capitalize()
+    nome = nome +"."
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(nome)
+    time.sleep(20)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
